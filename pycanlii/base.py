093388f4309fc7e6b53cf2bc37917823f704f47c
Jonathan Webb <jwebbed@bell.net>
2014-10-21 15:51:31 -0400

    Wrote base class

---
+++ pycanlii/base.py
@@ -0,0 +1,43 @@
+__author__ = 'ur mom'
+
+import requests
+import json
+import os
+
+class PyCanliiBase:
+
+    def __init__(self, apikey, language="en"):
+        self.key = apikey
+        self.lang = language
+
+
+    def request(self, url, authenticated, *url_variables, **query_parameters):
+        '''
+        Sends a request to the input url, with the url parameters
+        place in order with a / between each and with the
+        query parameters input
+        :param url:
+        :param authenticated: A boolean representing if this request
+        should be authenticated or not
+        :param url_variables:
+        :param query_parameters:
+        :return:
+        '''
+        if authenticated:
+            query_parameters['api_key'] = self.key
+
+        url += "/" + self.lang
+        for var in url_variables:
+            url += "/" + var
+
+        if (len(query_parameters)):
+            return requests.get(url, params=query_parameters)
+        else:
+            return requests.get(url)
+
+if __name__ == '__main__':
+    x = PyCanliiBase(os.environ["CANLII_KEY"])
+    #y = x.request("http://api.canlii.org/v1/legislationBrowse", True)
+    #print(y.json())
+    y = x.request("http://api.canlii.org/v1/legislationBrowse", True, 'cas')
+    print(y)
