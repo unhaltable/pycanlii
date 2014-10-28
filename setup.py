d47fa5752199134ba26b3dc3de2564d7afdb1c48
Jonathan Webb <jonathan@jonathan-eos.(none)>
2014-10-28 02:36:13 -0400

    updated the setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "pycanlii",
+    packages = ["pycanlii"],
+    version = "0.4",
+    author = "Sherlocke",
+    install_requires=["beautifulsoup4", "requests"]
+    )
