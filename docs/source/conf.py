9cb1c9c92f04d466193954412b4d6fba832ee7fb
Jonathan Webb <jwebbed@bell.net>
2014-10-29 19:41:16 -0400

    maybe this works

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.insert(0, "/Users/jonathanwebb/Projects/pycanlii")
+sys.path.insert(0, "/pycanlii")
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
