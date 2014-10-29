ab32d8e3c5d5a0f6d7e0c18b154c2fcc521db82c
Jonathan Webb <jonathan@jonathan-eos.(none)>
2014-10-29 19:30:29 -0400

    Fixed bug in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,43 +21,48 @@ def canlii(config):
     return CanLII(config['key'], pycanlii.enumerations.Language.en)
 
 @pytest.fixture
-def case_fr(config):
-    data = {
+def case_fr():
+    return {
         'databaseId': 'qccs',
         'caseId': { 'fr' : '2008qccs3554' },
         'title' : 'Société 3505 Ste-Famille inc. c. Régie du logement',
         'citation' : '2008 QCCS 3554 (CanLII)'
     }
-    return Case(data, config['key'], pycanlii.enumerations.Language.en)
 
 @pytest.fixture
-def case_en(config):
-    data = {
+def case_en():
+    return {
         'databaseId': 'csc-scc',
         'caseId': { 'en' : '2008scc9' },
         'title' : 'Dunsmuir v. New Brunswick',
         'citation' : '[2008] 1 SCR 190, 2008 SCC 9 (CanLII)'
     }
-    return Case(data, config['key'], pycanlii.enumerations.Language.en)
 
 @pytest.fixture
-def legis_en(config):
-    data = {
+def case(case_en, config):
+    return Case(case_en, config['key'], pycanlii.enumerations.Language.en)
+
+@pytest.fixture
+def legis_en():
+    return {
       "databaseId": "car",
       "legislationId": "sor-86-946",
       "title": "Abatement of Duties Payable Regulations",
       "citation": "SOR/86-946",
       "type": "REGULATION"
     }
-    return Legislation(data, config['key'], pycanlii.enumerations.Language.en)
 
 @pytest.fixture
-def legis_fr(config):
-    data = {
+def legis_fr():
+    return {
       "databaseId": "car",
       "legislationId": "dors-96-383",
       "title": "Décret sur l\u0027abandon et la poursuite des procédures, 1996",
       "citation": "DORS/96-383",
       "type": "REGULATION"
     }
-    return Legislation(data, config['key'], pycanlii.enumerations.Language.fr)
+
+@pytest.fixture
+def legislation(legis_en, config):
+    return Case(legis_en, config['key'], pycanlii.enumerations.Language.en)
+
