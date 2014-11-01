e5594b5e7efa5347708582125bd4dbce128aacb7
Jonathan Webb <jwebbed@bell.net>
2014-11-01 02:38:37 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name = "pycanlii",
+    description = "A python wrapper for the CanLII API",
     packages = ["pycanlii"],
-    version = "0.4",
-    author = "Sherlocke",
+    version = "0.9.0",
+    author = "Jonathan Webb",
     install_requires=["beautifulsoup4", "requests"]
     )
