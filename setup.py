34f585fee8a17aa6b3fffd3278eab9635ebca9c8
Jonathan Webb <jwebbed@bell.net>
2014-11-01 20:35:06 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name = "pycanlii",
     description = "A python wrapper for the CanLII API",
     packages = ["pycanlii"],
-    version = "1.0.0",
+    version = "1.0.0b1",
     author = "Jonathan Webb",
+    home_page = "http://sherlocke.me/developers/",
     install_requires=["requests"]
     )
